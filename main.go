f1e88bec6c3ef4c7e9e555dbff6bb825acae59d4
Szymon Mikitiuk <szymon.mikitiuk@infermedica.com>
2024-02-03 22:23:31 +0100

    Add shuffling of player's input (quiz part #1)

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
+	"time"
 )
 
 var (
@@ -14,17 +16,21 @@ var (
 )
 
 func main() {
-	fileName, _ := parseUserInput()
+	fileName, _, shuffle := parseUserInput()
 	data := readQuestions(fileName)
+	if shuffle {
+		scramble(data)
+	}
 	mainLoop(data)
 	fmt.Printf("You scored %d out of %d.\n", correctAnswers, correctAnswers+wrongAnswers)
 }
 
-func parseUserInput() (string, int) {
+func parseUserInput() (string, int, bool) {
 	csvPath := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
 	timeLimit := flag.Int("limit", 30, "the time limit for the quiz in seconds")
+	shuffle := flag.Bool("shuffle", false, "shuffle the order of questions")
 	flag.Parse()
-	return *csvPath, *timeLimit
+	return *csvPath, *timeLimit, *shuffle
 }
 
 func readQuestions(fileName string) [][]string {
@@ -32,6 +38,7 @@ func readQuestions(fileName string) [][]string {
 	if err != nil {
 		log.Fatalf("Error: %v", err)
 	}
+	defer file.Close()
 
 	csvReader := csv.NewReader(file)
 	data, err := csvReader.ReadAll()
@@ -41,6 +48,14 @@ func readQuestions(fileName string) [][]string {
 	}
 
 	return data
+}
+
+func scramble(data [][]string) {
+	source := rand.NewSource(time.Now().UnixNano())
+	random := rand.New(source)
+	random.Shuffle(len(data), func(i, j int) {
+		data[i], data[j] = data[j], data[i]
+	})
 }
 
 func mainLoop(data [][]string) {
